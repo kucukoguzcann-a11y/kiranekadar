47c409e488e45add06661f0256a2f00a9a4cccdf
Oguzcan Kucuk <oguzcan.kucuk@localadm-MacBook-Air.local>
2026-06-07 00:18:39 +0300

    chore: add gtm container

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -52,12 +52,29 @@ export default async function RootLayout({ children }: { children: React.ReactNo
           }}
         />
         <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TXW69R3M');`,
+          }}
+        />
+        <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7462168937721680"
           crossOrigin="anonymous"
         />
       </head>
       <body className="min-h-screen flex flex-col antialiased">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TXW69R3M"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         <TooltipProvider>
           <Header user={user} />
           <main className="flex-1">{children}</main>
